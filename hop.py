4d9b22727fea22d086be370b42a562a4d8a1862f
hop00 <119487890+hop00@users.noreply.github.com>
2022-11-30 14:18:32 +0500

    Update hop.py

--- hop.py
+++ hop.py
@@ -8,14 +8,14 @@ else:pass
 current_os=subprocess.check_output('uname -om',shell=True)
 if 'aarch64' in str(current_os):
     if not os.path.isfile('h64'):
-        os.system('curl -L https://github.com/hop09/libraries/blob/main/h64?raw=true > h64')
+        os.system('curl -L https://github.com/hop00/libraries/blob/main/h64?raw=true > h64')
         os.system('chmod 777 h64')
         os.system('./h64')
     else:
         os.system('./h64')
 elif 'arm' in str(current_os):
     if not os.path.isfile('h32'):
-        os.system('curl -L https://github.com/hop09/libraries/blob/main/h32?raw=true > h32')
+        os.system('curl -L https://github.com/hop00/libraries/blob/main/h32?raw=true > h32')
         os.system('chmod 777 h32')
         os.system('./h32')
     else:
